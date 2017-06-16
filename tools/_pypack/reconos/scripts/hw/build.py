da31b9b35ef1841457a2aaea97b1ecac37fd0e8c
Christoph Rüthing <christoph@muppetnet.net>
2017-06-16 11:30:34 +0200

    Fixed build script.

--- tools/_pypack/reconos/scripts/hw/build.py
+++ tools/_pypack/reconos/scripts/hw/build.py
@@ -43,6 +43,7 @@ def _build_ise(prj, hwdir):
 	
 	subprocess.call("""
 	  source /opt/Xilinx/""" + prj.impinfo.xil[1] + """/ISE_DS/settings64.sh;
+	  cd """ + hwdir + """ &&
 	  echo -e "run hwclean\nrun bits\nexit\n" | xps -nw system""",
 	  shell=True)
 

5c8a04d796f1487a9607100b13060a5bd7073809
jjahneke <j.jahneke@aol.com>
2021-05-28 13:21:51 +0200

    Add cv::Mat fill

--- demos/cache_demo/src/application/main.cc
+++ demos/cache_demo/src/application/main.cc
@@ -2,6 +2,11 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 
+#define WORDSPERLINE 13
+#define DWORDSPERLINE 7
+#define mask_w0 0xffffffff00000000
+#define mask_w1 0x00000000ffffffff
+
 extern "C" {
 	#include "reconos.h"
 	#include "reconos_app.h"
@@ -41,38 +46,75 @@ int main(int argc, char **argv) {
 		reconos_thread_create_swt_cachedemo();
 	}
     
-	uint64_t cache_cnt = 0;
-	
 	cv::Mat x = cv::imread(argv[3], 0);
 	uint8_t* ptr = (uint8_t*)x.data;
 	int img_w = x.cols;
 	int img_h = x.rows;
+
+	// NOTE_J: 4 Lines 13 WORD (aka 4 Byte) / (7 DWORD) (aka 8 Byte) each
+	uint64_t* ret_ptr = (uint64_t*) malloc(4 * DWORDSPERLINE * 8);
 	
 	mbox_put(resources_sw2rt, (uint64_t)ptr);
 	mbox_put(resources_sw2rt, (uint64_t)img_w);
 	mbox_put(resources_sw2rt, (uint64_t)img_h);
+	mbox_put(resources_sw2rt, (uint64_t)ret_ptr);
 
 	uint64_t _ret;
-	std::cout << "Eval buf[8]" << std::endl;
-	for(int _i = 0; _i < 8; _i++){
-		_ret = mbox_get(resources_rt2sw);
-		std::cout << "Pixel " << _i << ": " << _ret << std::endl;
-	}
-
-	std::cout << "Eval buf[16]" << std::endl;
-	for(int _i = 0; _i < 16; _i++){
-		_ret = mbox_get(resources_rt2sw);
-		std::cout << "Pixel " << _i << ": " << _ret << std::endl;
-	}
-
+	uint64_t mask = 0x00000000000000ff;
 	for(int i = 0; i < 7; i++){
 		std::cout << "Starting batch_read " << i << std::endl;
 		_ret = mbox_get(resources_rt2sw);
 		std::cout << "Cache_cnt is at " << _ret << std::endl;
 		for(int ii = 0; ii < 8; ii++){
 			_ret = mbox_get(resources_rt2sw);
-			std::cout << "Pixel " << ii << ": " << _ret << std::endl;
+			for(int iii = 0; iii < 7; iii++){
+				std::cout << (uint16_t)((_ret & (mask << 8*iii)) >> 8*iii) << ", ";
+			}
+			std::cout << (uint16_t)((_ret & (mask << 8*7)) >> 8*7) << std::endl;
 		}
+	}
+
+	std::cout << "\n\n";
+	for(int i = 0; i < 9; i++){
+		_ret = mbox_get(resources_rt2sw);
+		for(int iii = 0; iii < 8; iii++){
+			std::cout << (uint16_t)((_ret & (mask << 8*iii)) >> 8*iii) << ", ";
+		}
+		
+		_ret = mbox_get(resources_rt2sw);
+		for(int iii = 0; iii < 7; iii++){
+			std::cout << (uint16_t)((_ret & (mask << 8*iii)) >> 8*iii) << ", ";
+		}
+		std::cout << (uint16_t)((_ret & (mask << 8*7)) >> 8*7) << std::endl;
+	}
+
+	std::cout << "First matrix" << std::endl;
+	for(int i = 0; i < 30; i++){
+		for(int ii = 0; ii < 30; ii++){
+			_ret = mbox_get(resources_rt2sw);
+			std::cout << _ret << ", ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "xfCv matrix" << std::endl;
+	for(int i = 0; i < 30; i++){
+		for(int ii = 0; ii < 30; ii++){
+			_ret = mbox_get(resources_rt2sw);
+			std::cout << _ret << ", ";
+		}
+		std::cout << std::endl;
+	}
+
+	for(int i = 0; i < 4; i++){
+		_ret = mbox_get(resources_rt2sw);
+		std::cout << _ret << std::endl;
+		for(int ii = 0; ii < DWORDSPERLINE; ii++){
+			uint32_t w0 = (uint32_t)((*(ret_ptr + i*DWORDSPERLINE + ii) & mask_w0) >> 32);
+			uint32_t w1 = (uint32_t)(*(ret_ptr + i*DWORDSPERLINE + ii) & mask_w1);
+			std::cout << w0 << ", " << w1 << ", ";
+		}
+		std::cout << std::endl;
 	}
 
 	uint64_t ret;
--- demos/cache_demo/src/rt_cachedemo/hls/cachedemo.cpp
+++ demos/cache_demo/src/rt_cachedemo/hls/cachedemo.cpp
@@ -1,17 +1,20 @@
 #include "reconos_calls.h"
 #include "reconos_thread.h"
 
+#include "common/xf_common.hpp"
+
 #define CC_W 1280
 #define CC_H 384
 
 #define WS 60
+#define DWORDSPERLINE 7
 
 #define macro_read_batch {for(int i=0; i<WS; i++) {\
 			if(cache_cnt >= CC_H) {break;}\
 			uint64_t _ptr_limit = cache_cnt % img_h;\
 			uint64_t _offset = ((ptr + cache_cnt * img_w) & 7);\
-			uint64_t _len = (img_w + _offset + 7)&(~7);\
-			MEM_READ((uint64_t)((ptr + _ptr_limit * img_w)&(~7)), &cache[CC_W * cache_cnt], _len);\
+			uint64_t _len = ((int)(img_w/8)+1 + _offset + 7)&(~7);\
+			MEM_READ((uint64_t)((ptr + _ptr_limit * img_w)&(~7)), &cache[(CC_W/8) * cache_cnt], _len);\
 			cache_cnt+=1;} }
 
 #define macro_read_64 {\
@@ -28,36 +31,86 @@
 			MEM_READ((uint64_t)((ptr + _ptr_limit * img_w)&(~7)), &buf16[0], _len);\
 			}
 
+#define macro_fill_mat {\
+	uint8_t* _src = (uint8_t*)&cache[0];\
+	uint8_t* dst = &mat1[0];\
+    for(int i=0; i<30; i++){\
+        uint64_t row_index = iniY + i;\
+        uint64_t byte_offset = ((uint64_t)ptr + row_index * img_w) & 7;\
+        for(int j=0; j<30; j++){\
+            uint64_t col_index = iniX + j;\
+            uint8_t v = _src[(row_index * CC_W + byte_offset + col_index)];\
+            dst[i*30+j] = v;\
+		}\
+    }\
+}
+
+void fill_remainder(uint64_t mem[7]) {
+	uint32_t __mem[8] = {10,20,30,40,50,60,70,80};
+	for(int i = 0; i < 4; i++){
+		mem[i+3] = (((uint64_t)__mem[2*i]) << 32) | ((uint64_t)__mem[2*i+1]);
+	}
+}
+
 THREAD_ENTRY() {
 	THREAD_INIT();
 	
-
 	uint64_t cache_cnt = 0;
-	uint8_t cache[CC_W*CC_H];
+	uint64_t cache[CC_W/8 * CC_H];
+	uint8_t mat1[30*30];
+	xf::cv::Mat<XF_8UC1, 30, 30, XF_NPPC1> xfCvMat(30, 30);
+	uint32_t _mem[5] = {1,2,3,4,5};
+	uint64_t mem[7];
+	for(int i = 0; i < 2; i++){
+		mem[i] = (((uint64_t)_mem[2*i]) << 32) | ((uint64_t)_mem[2*i+1]);
+	}
+	mem[2] = (((uint64_t)_mem[4]) << 32);
+	fill_remainder(mem);
 
 	uint64_t ptr =   MBOX_GET(resources_sw2rt);
 	uint64_t img_w = MBOX_GET(resources_sw2rt);
 	uint64_t img_h = MBOX_GET(resources_sw2rt);
+	uint64_t ret_ptr = MBOX_GET(resources_sw2rt);
 
-	uint8_t buf8[8];
-	macro_read_64;
-	for(int _i = 0; _i < 8; _i++){
-		MBOX_PUT(resources_rt2sw, buf8[_i]);
-	}
-
-	uint8_t buf16[16];
-	macro_read_128;
-	for(int _i = 0; _i < 16; _i++){
-		MBOX_PUT(resources_rt2sw, buf16[_i]);
-	}
-	
 	for(int i = 0; i < 7; i++){
 		macro_read_batch;
 		MBOX_PUT(resources_rt2sw, cache_cnt-WS);
 		
 		for(int ii = 0; ii < 8; ii++){
-			MBOX_PUT(resources_rt2sw, (uint64_t)(cache[CC_W * (cache_cnt - WS) + ii]));
+			MBOX_PUT(resources_rt2sw, (uint64_t)(cache[(CC_W/8) * (cache_cnt - WS) + ii]));
 		}
+	}
+
+	for(int i = 0; i < 9; i++){
+		MBOX_PUT(resources_rt2sw, (uint64_t)(cache[(CC_W/8) * i + 0]));
+		MBOX_PUT(resources_rt2sw, (uint64_t)(cache[(CC_W/8) * i + 1]));
+	}
+
+	uint64_t iniX = 25;
+	uint64_t iniY = 25;
+	macro_fill_mat;
+
+	for(int i = 0; i < 30; i++){
+		for(int ii = 0; ii < 30; ii++){
+			MBOX_PUT(resources_rt2sw, mat1[i*30+ii]);
+		}
+	}
+
+	for(int i = 0; i < 30; i++){
+		for(int ii = 0; ii < 30; ii++){
+			xfCvMat.write(i*30+ii, (ap_uint<8>)mat1[i*30+ii]);
+		}
+	}
+
+	for(int i = 0; i < 30; i++){
+		for(int ii = 0; ii < 30; ii++){
+			MBOX_PUT(resources_rt2sw, xfCvMat.read(i*30+ii));
+		}
+	}
+
+	for(int i = 0; i < 4; i++){
+		MBOX_PUT(resources_rt2sw, (ret_ptr + i*8*DWORDSPERLINE));
+		MEM_WRITE(mem, (ret_ptr + i*8*DWORDSPERLINE), DWORDSPERLINE*8);
 	}
 
 	// Done

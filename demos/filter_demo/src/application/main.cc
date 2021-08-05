6889cf1c716c637dd3955d15679136f51a4cb93b
jjahneke <j.jahneke@aol.com>
2021-08-05 11:47:26 +0200

    Add filterdemo

---
+++ demos/filter_demo/src/application/main.cc
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <fstream>
+
+#if 0 // ReconOS64
+    #define BASETYPE uint64_t
+    #define BYTES 8
+    #define MASK 7
+	const BASETYPE _DONE = 0xffffffffffffffff;
+#else // ReconOS32
+    #define BASETYPE uint32_t
+    #define BYTES 4
+    #define MASK 3
+	const BASETYPE _DONE = 0xffffffff;
+#endif
+
+
+extern "C" {
+	#include "reconos.h"
+	#include "reconos_app.h"
+	#include "timer.h"
+}
+
+#define CC_W 1280
+#define CC_H 384
+
+void print_help() {
+	std::cout <<
+		"Usage:  filterdemo <sw_or_hw> <mode> <img_in> <img_out>\n"
+		"sw_or_hw: {0: sw, 1: hw}\n"
+		"mode: {0: Dirac/Unit, 1: Gaussian 3x3, 1: Sobel X&Y}"
+	<< std::endl;
+}
+
+int main(int argc, char **argv) {
+	int sw_or_hw, mode;
+	int clk;
+
+	if (argc != 5) {
+		print_help();
+		return 0;
+	}
+
+	sw_or_hw = atoi(argv[1]);
+	mode = atoi(argv[2]);
+
+	reconos_init();
+	reconos_app_init();
+	timer_init();
+	clk = reconos_clock_threads_set(100000);
+
+	if(sw_or_hw == 0) { 
+		std::cout << "Creating SW Thread" << std::endl;
+		reconos_thread_create_swt_filterdemo();
+	}
+	else { 
+		std::cout << "Creating HW Thread" << std::endl;
+		reconos_thread_create_hwt_filterdemo();
+	}
+
+	// Open image, allocate output memory
+	cv::Mat img_i = cv::imread(argv[3], 0);
+	int rows = img_i.rows;
+	int cols = img_i.cols;
+	// Append zero'd row to compensate last line over-read
+	cv::Mat _tmp = cv::Mat::zeros(1,cols,img_i.type());
+	img_i.push_back(_tmp);
+
+	cv::Mat img_o = cv::Mat::zeros(rows, cols, CV_8U);
+	uint8_t* ptr_i = (uint8_t*)img_i.data;
+	uint8_t* ptr_o = (uint8_t*)calloc(CC_W * CC_H, sizeof(uint8_t));
+
+	// Do thread work
+	std::cout << "Starting thread work" << std::endl;
+	unsigned int t_start, t_end;
+	t_start = timer_get();
+	{
+		BASETYPE ret;
+
+		mbox_put(rcs_sw2rt, (BASETYPE)ptr_i);
+		mbox_put(rcs_sw2rt, (BASETYPE)cols);
+		mbox_put(rcs_sw2rt, (BASETYPE)rows);
+		mbox_put(rcs_sw2rt, (BASETYPE)ptr_o);
+		mbox_put(rcs_sw2rt, (BASETYPE)mode);
+
+		do {
+			ret = mbox_get(rcs_rt2sw);
+			std::cout << "Before MEM_WRITE to addr " << (uint64_t*)ret << std::endl;
+		}
+		while(ret != _DONE);
+	}
+	t_end = timer_get();
+	std::cout << "Done with thread work!" << std::endl;
+	std::cout << "Compute took " << timer_toms(t_end - t_start) << " ms" << std::endl;
+	std::cout << "Pointer to mem is " << (BASETYPE*)ptr_o << std::endl;
+
+	// Create image from allocated memory
+	for(int row = 0; row < rows; row++) {
+	    uint8_t* row_ptr = (uint8_t*)img_o.ptr(row);
+	    memcpy(row_ptr, ptr_o + (row)*CC_W, cols);
+	}
+
+	// Store image
+	cv::imwrite(argv[4], img_o);
+	std::cout << "Image written to " << argv[4] << std::endl;
+	
+	// Cleanup
+	free(ptr_o);
+	ptr_o = NULL;
+
+	reconos_app_cleanup();
+	reconos_cleanup();
+	
+	return 0;
+}
---
+++ demos/filter_demo/src/rt_filterdemo/hls/filterdemo.cpp
@@ -0,0 +1,145 @@
+#include "reconos_calls.h"
+#include "reconos_thread.h"
+#include "hls_math.h"
+
+#if 0 // ReconOS64
+    #define BASETYPE uint64_t
+    #define BYTES 8
+    #define MASK 7
+	const BASETYPE _DONE = 0xffffffffffffffff;
+#else // ReconOS32
+    #define BASETYPE uint32_t
+    #define BYTES 4
+    #define MASK 3
+	const BASETYPE _DONE = 0xffffffff;
+#endif
+
+#define CC_W 1280
+#define CC_H 384
+const BASETYPE BYTEMASK = 0xff;
+
+// Filter properties
+#define FILTER_SIZE 3
+#define FILTER_SIZE_H ((int)(FILTER_SIZE / 2))
+#define CACHE_LINES (FILTER_SIZE + 1)
+#define PREFETCH_ROWS (CACHE_LINES - 1)
+
+// Unit kernel for verification
+const uint8_t filterU[] = {0, 0, 0,
+						   0, 1, 0,
+						   0, 0, 0};
+
+// Discrete approximation for Gaussian 3x3 kernel
+// Divide by 16 -> shift right 4
+#define SHIFT_NORM_GAUSS 4
+const uint8_t filterG[] = {1,2,1,
+						  2,4,2,
+						  1,2,1};
+
+#define SHIFT_NORM_SOBEL 3
+const int8_t filterX[] = { 1, 2, 1,
+						    0, 0, 0,
+						   -1,-2,-1};
+
+const int8_t filterY[] = { 1, 0, -1,
+						   2, 0, -2,
+						   1, 0, -1};
+
+#define macro_prefetch_rows {\
+	for(int i = 0; i < PREFETCH_ROWS; i++) {\
+		BASETYPE _offset = ((ptr_i + i * img_w) & MASK);\
+		BASETYPE _len = (img_w + _offset + BYTES)&(~MASK);\
+		MEM_READ(((ptr_i + i * img_w)&(~MASK)), &_in[0], _len);\
+		for(int ii = 0; ii < CC_W; ii++) {\
+			uint8_t _byte_in_dword = (uint8_t)((_offset + ii) % BYTES);\
+			uint16_t _dword_ptr = (uint16_t)((_offset + ii) / BYTES);\
+			BASETYPE _dword = _in[_dword_ptr];\
+			uint8_t _byte = ((_dword & (BYTEMASK << _byte_in_dword*8))>> _byte_in_dword*8);\
+			cache[i*CC_W + ii] = _byte;\
+		}\
+	}\
+}
+
+#define macro_read_row {\
+	BASETYPE ptr_limit = (row-FILTER_SIZE_H) % img_h;\
+	BASETYPE _offset = ((ptr_i + (PREFETCH_ROWS + (ptr_limit)) * img_w) & MASK);\
+	BASETYPE _len = (img_w + _offset + BYTES)&(~MASK);\
+	MEM_READ(((ptr_i + (PREFETCH_ROWS + (ptr_limit)) * img_w)&(~MASK)), &_in[0], _len);\
+	for(int ii = 0; ii < CC_W; ii++) {\
+		uint8_t _byte_in_dword = (uint8_t)((_offset + ii) % BYTES);\
+		uint16_t _dword_ptr = (uint16_t)((_offset + ii) / BYTES);\
+		BASETYPE _dword = _in[_dword_ptr];\
+		uint8_t _byte = ((_dword & (BYTEMASK << _byte_in_dword*8))>> _byte_in_dword*8);\
+		BASETYPE _cache_line = CC_W * ((row+FILTER_SIZE_H+1) % CACHE_LINES);\
+		cache[_cache_line + ii] = _byte;\
+	}\
+}
+
+#define macro_write_row {\
+	MEM_WRITE(&_out[0], (ptr_o + row*CC_W), CC_W);\
+}
+
+THREAD_ENTRY() {
+	THREAD_INIT();
+
+	uint8_t cache[CC_W * CACHE_LINES];
+	BASETYPE _in[CC_W/BYTES + 1];
+	BASETYPE _out[CC_W/BYTES];
+
+	BASETYPE ptr_i = MBOX_GET(rcs_sw2rt);
+	BASETYPE img_w = MBOX_GET(rcs_sw2rt);
+	BASETYPE img_h = MBOX_GET(rcs_sw2rt);
+	BASETYPE ptr_o = MBOX_GET(rcs_sw2rt);
+	BASETYPE mode = MBOX_GET(rcs_sw2rt);
+
+	// Prefetch PREFETCH_ROWS lines of image
+	macro_prefetch_rows;
+	for(int row = FILTER_SIZE_H; row < img_h - FILTER_SIZE_H; row++) {
+		macro_read_row;
+		for(int i = 0; i < CC_W/BYTES; i++) {
+			_out[i] = 0;
+		}
+	
+		for(int col = FILTER_SIZE_H; col < img_w - FILTER_SIZE_H; col++) {
+			// Reset temporary accumulation buffer
+			uint16_t res = 0;
+			int16_t resX = 0;
+			int16_t resY = 0;
+
+			uint16_t filter_ptr = 0;
+			for(int i = -FILTER_SIZE_H; i <= FILTER_SIZE_H; i++) {
+				for(int j = -FILTER_SIZE_H; j <= FILTER_SIZE_H; j++) {
+					uint8_t _byte = cache[(row+i)%CACHE_LINES * CC_W + (col+j)];
+					if(mode == 0) {
+						res += _byte * filterU[filter_ptr];
+					}
+					else if(mode == 1) {
+						res += _byte * filterG[filter_ptr];
+					}
+					else {
+						resX += _byte * filterX[filter_ptr];
+						resY += _byte * filterY[filter_ptr];
+					}
+					filter_ptr++;
+				}
+			}
+			
+			// Normalize result
+			if(mode == 0) {
+				_out[col/BYTES] |= ((BASETYPE)res) << 8*(col&MASK);
+			}
+			else if(mode == 1) {
+				_out[col/BYTES] |= (((BASETYPE)(res >> SHIFT_NORM_GAUSS)) << 8*(col&MASK));
+			}
+			else {
+				_out[col/BYTES] |= (((BASETYPE)(((uint16_t)hls::abs(resX) + (uint16_t)hls::abs(resY)) >> SHIFT_NORM_SOBEL)) << 8*(col&MASK));
+			}
+		}
+		// Write-back computed row
+		BASETYPE _addr = ptr_o + row*CC_W;
+		MBOX_PUT(rcs_rt2sw, _addr);
+		macro_write_row;
+	}
+
+	MBOX_PUT(rcs_rt2sw, _DONE);
+}
